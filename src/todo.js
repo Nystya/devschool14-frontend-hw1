9309b83260b0a2a1a34c9c3252e4823d5a741606
cvajaiala <catalin.vajaiala@gmail.com>
2020-12-03 20:06:31 +0200

    Added Axios to fetch todo list

--- src/todo.js
+++ src/todo.js
@@ -5,35 +5,76 @@ import {
 class Todo extends LitElement {
     static get styles() {
         return css`
-                .todo-checked {
-                    color: red;
-                }
-            `;
+          .todo-checked {
+            color: red;
+          }
+        `;
     }
 
     static get properties() {
         return {
+            id: {type: Number},
             title: {type: String},
-            done: {type: Boolean}
+            done: {type: Boolean},
+
+            editing: {type: Boolean},
         }
     }
 
     constructor() {
         super();
+
+        this.editing = false;
     }
 
     render() {
         return html`
-                <div class="${this.done ? "todo-checked" : ""}">
-                    ${this.title} 
-                    <input type="checkbox" ?checked="${this.done}" @click="${this.handleCheckboxClick}"/>
-                </div>
-            `;
+            <div class="${this.done ? "todo-checked" : ""}">
+                ${this.editing ?
+                        html`
+                            <input value="${this.title}" id="updatedTitle" name="updatedTitle"/>
+                            <button @click="${this.handleSaveClick}">Save</button>
+                            <button @click="${this.handleCancelClick}">Cancel</button>
+                        `
+                        :
+                        html`
+                            ${this.title}
+                            <input type="checkbox" ?checked="${this.done}" @click="${this.handleCheckboxClick}"/>
+                            <button @click="${this.handleEditClick}">Edit</button>
+                        `
+                }
+
+                
+            </div>
+        `;
     }
 
     handleCheckboxClick(event) {
-        console.log(event);
-        this.dispatchEvent(event);
+        this.dispatchEvent(new CustomEvent("checkboxClickEvent", {
+            detail: {
+                id: this.id,
+                done: event.target.checked,
+            }
+        }));
+    }
+
+    handleEditClick(event) {
+        this.editing = true;
+    }
+
+    handleSaveClick(event) {
+        this.editing = false;
+
+        this.dispatchEvent(new CustomEvent("updateTodoEvent", {
+            detail: {
+                id: this.id,
+                title: this.shadowRoot.querySelector("#updatedTitle").value
+            }
+        }));
+    }
+
+    handleCancelClick(event) {
+        this.editing = false;
     }
 }
 
--- src/todos.js
+++ src/todos.js
@@ -1,6 +1,4 @@
-import {
-    LitElement, html, css
-} from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
+import {html, LitElement} from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
 
 import Todo from "./todo.js";
 
@@ -17,30 +15,111 @@ class Todos extends LitElement {
     constructor() {
         super();
 
+        this.postsLink = "https://jsonplaceholder.typicode.com/todos"
+        this.axios = window.axios;
+
         this.title = "Titlu";
-        this.todos = [
-            {
-                title: "Wash Dishes",
-                done: true
-            },
-            {
-                title: "Do Laundry",
-                done: false
-            }
-        ];
+
+        this.todos = []
+
+        this.getPosts()
     }
 
     render() {
+        console.log("rendering")
         return html`
-        <h2> ${this.title} </h2>
-        <ul>
-            ${this.todos.map((todo, index) => {
-                return html`
-                    <my-todo .title="${todo.title}" .done="${todo.done}"></my-todo>
-                `
-            })}           
-        </ul>
+            <h2> ${this.title} </h2>
+            
+            <form @submit="${this.handleTodoSubmit}">
+                <input type="text" placeholder="New Todo" name="todoTitle" id="todoTitle">
+                <input type="submit" value="Add">
+            </form>
+            
+            <ul>
+                ${this.todos.map((todo, index) => {
+                    return html`
+                        <my-todo 
+                                .id="${index}" 
+                                .title="${todo.title}" 
+                                .done="${todo.done}"
+                                @checkboxClickEvent="${this.handleCheckboxClickEvent}"
+                                @updateTodoEvent="${this.handleUpdateTodoEvent}"
+                        >
+                        </my-todo>
+                    `
+                })}
+            </ul>
         `;
+    }
+
+    getPosts() {
+        this.axios
+            .get(this.postsLink)
+            .then(response => {
+                let newTodos = []
+
+                response.data.forEach((item => {
+                    newTodos.push({title: item.title, done: item.completed})
+
+                }))
+
+                this.todos = newTodos
+            })
+            .catch(err => {
+                console.log(err)
+            })
+            .finally(() => {
+                console.log(this.todos)
+            })
+    }
+
+    addPost() {
+        this.axios
+            .post(this.postsLink,
+                {
+                    title: "foo",
+                    completed: false,
+                    userId: 1,
+                },
+                {
+                    headers: {
+                        "content-type": "form-data/multipart",
+                        Autorization: 'Bearer token'
+                    }
+                }
+            )
+            .then(response => {
+                console.log(response)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    handleCheckboxClickEvent(event) {
+        const id = event.detail.id
+        const done = event.detail.done
+
+        let newTodos = [...this.todos]
+        newTodos[id].done = done
+        this.todos = newTodos
+    }
+
+    handleTodoSubmit(event) {
+        event.preventDefault()
+
+        const formData = new FormData(event.target)
+
+        this.todos = [...this.todos, {title: formData.get("todoTitle"), done: false}]
+    }
+
+    handleUpdateTodoEvent(event) {
+        const id    = event.detail.id
+        const title = event.detail.title
+
+        let newTodos = [...this.todos]
+        newTodos[id].title = title
+        this.todos = newTodos
     }
 }
 
